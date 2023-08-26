cace3f17af5a37cc4a84960b1699fe7fee0436b3
James Sinclair <jsinclair2@atlassian.com>
2023-08-26 10:56:16 +1000

    🎉 Initial setup with first few slides

---
+++ src/presentation.ts
@@ -0,0 +1,12 @@
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown';
+import Notes from 'reveal.js/plugin/notes/notes.esm';
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm';
+
+let deck = new Reveal({
+  plugins: [Markdown, Notes, Highlight],
+});
+deck.initialize({
+  width: 1792,
+  height: 1120,
+});
